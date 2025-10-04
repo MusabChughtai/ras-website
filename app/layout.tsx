fe0583f04e2eaed65d5eb8790917f3d89a914278
MusabChughtai <musab114c@gmail.com>
2025-10-04 17:02:13 +0500

    feat: Update favicon and enhance web app manifest with additional properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,7 @@ export const metadata: Metadata = {
     { rel: "icon", type: "image/x-icon", sizes: "any", url: "/favicon.ico" },
     { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
     { rel: "shortcut icon", url: "/favicon.ico" },
+    { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" }
   ],
   manifest: "/site.webmanifest",
   appleWebApp: {
