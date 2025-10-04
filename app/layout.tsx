aceb1278c7491d2d8ba4eb77bb927a0ae22bddcc
MusabChughtai <musab114c@gmail.com>
2025-10-04 17:06:21 +0500

    feat: Update favicon configuration in layout.tsx for improved icon support

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,11 +100,11 @@ export const metadata: Metadata = {
     nocache: false,
   },
   icons: [
+    { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon-96x96.png" },
     { rel: "icon", type: "image/svg+xml", url: "/favicon.svg" },
-    { rel: "icon", type: "image/x-icon", sizes: "any", url: "/favicon.ico" },
-    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
     { rel: "shortcut icon", url: "/favicon.ico" },
-    { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" }
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    { rel: "icon", type: "image/x-icon", sizes: "any", url: "/favicon.ico" },
   ],
   manifest: "/site.webmanifest",
   appleWebApp: {
